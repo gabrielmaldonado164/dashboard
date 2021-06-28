2c08d1839abf97149bf5e2079a01ca657d727bb0
Gabriel Maldonado <gabriel.maldonado@towebs.com>
2021-06-28 08:51:07 -0300

    addded ..

--- panel/core/erp/models/category.py
+++ panel/core/erp/models/category.py
@@ -8,6 +8,9 @@ from django.db                          import models
 from core.erp.models.models_auditoria   import AuditoriaModel
 from django.utils.translation           import ugettext_lazy as _
 from django.forms                       import model_to_dict
+from crum                             import get_current_user
+
+
 
 
 
@@ -22,6 +25,16 @@ class Category(AuditoriaModel):
     def toJson(self): #devuelve un diccionario con todo los datos, tambien puedo usar un exclude 
         item = model_to_dict(self)#convierte en diccionario los campos
         return item
+
+    #para auditoria se sobreescribe el metodo save para poder obtener las peticions de los usuarios en session
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        user = get_current_user()
+        if user is not None:
+            if not self.pk:
+                self.user_creation = user
+            else:
+                self.user_update = user
+        super().save(force_insert=force_insert, force_update=force_update, using=using, update_fields=update_fields)
 
     class Meta:
         verbose_name = 'Category'
